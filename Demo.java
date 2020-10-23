671b7f7c2003d5a63c61333baed93d32a4801b6c
Logan <lruzzier20@stuy.edu>
2020-10-22 22:28:25 -0400

    I should've committed earlier.

--- Demo.java
+++ Demo.java
@@ -2,6 +2,14 @@ public class Demo{
   public static void main(String[] args){
     String a = "3";
     if(args.length==0){printLoop(5);}else{printLoop(Integer.parseInt(args[0]));}
+    int[] b = new int[]{1,2,3,4,5,6};
+    System.out.println(arrToString(b));
+    int[][] c = new int[][]{
+      new int[]{1,2,3,4,5},
+      new int[]{6,7,8},
+      new int[]{4,1,0,12,33,13,53}};
+    System.out.println(arrDeepToString(create2DArray(3,3,6)));
+    System.out.println(arrDeepToString(create2DArrayRandomized(3,3,6)));
   }
 
   public static void printLoop(int n){
@@ -14,4 +22,50 @@ public class Demo{
       System.out.println();
     }
   }
+
+  public static String arrToString(int[] arr){
+    String sentence = "{";
+    for(int i=0;i<arr.length;i++){
+      sentence = sentence + (arr[i]);
+      if(i!=arr.length-1){sentence = sentence + ", ";}
+    }
+    sentence = sentence + "}";
+    return sentence;
+  }
+
+  public static String arrDeepToString(int[][] arr){
+    String sentence = "{";
+    for(int i=0;i<arr.length;i++){
+      sentence+="{";
+      for(int j=0;j<arr[i].length;j++){
+        sentence+=arr[i][j] + "";
+        if(j!=arr[i].length-1){sentence+= ", ";}
+      }
+      if(i==arr.length-1){sentence+="}";}else{sentence+="}, ";}
+    }
+    sentence+= "}";
+    return sentence;
+  }
+
+  public static int[][] create2DArray(int rows, int cols, int maxValue){
+    int[][] a = new int[rows][cols];
+    for(int i=0;i<a.length;i++){
+      for(int j=0;j<a[i].length;j++){
+        a[i][j] = (int) (Math.random() * (maxValue + 1));
+      }
+    }
+    return a;
+  }
+
+  public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+    int[][] a = new int[rows][];
+    for(int i=0;i<a.length;i++){
+      a[i] = new int[(int) (Math.random() * (cols + 1))];
+      for(int j=0;j<a[i].length;j++){
+        a[i][j] = (int) (Math.random() * (maxValue + 1));
+      }
+    }
+    return a;
+  }
+
 }
